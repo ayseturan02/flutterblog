5bbf38d624c8890f579809a9689207f34934f421
ayseturan02 <ayse0025turan@gmail.com>
2023-12-22 14:11:12 +0300

    I added buttons to the drawer section

--- lib/main.dart
+++ lib/main.dart
@@ -34,23 +34,56 @@ class WorkApp extends StatelessWidget {
          drawer: Drawer(
            backgroundColor: Colors.grey,
            child: ListView(
-             children: const [
-               DrawerHeader(
+             children:  [
+               const DrawerHeader(
                  decoration: BoxDecoration(
                    color: Colors.black,
                     image: DecorationImage(image: AssetImage("assets/image/a.png")),
                  ),
-                 child: Text(
-                  "sonra yazacaz",
-                 style: TextStyle(
-                   fontStyle: FontStyle.italic,
-                   color: Colors.white,
-                   fontSize: 12,
+                 child: Padding(
+                   padding: EdgeInsets.only(left: 0.0, top: 100.0,right: 0.0,bottom: 0.0),
+                   child: Text(
+                    "sonra yazacaz \n ksfsnnnnnnnnnnnnnnnnnnnnnn" ,
+                   style: TextStyle(
+                     fontStyle: FontStyle.italic,
+                     color: Colors.white,
+                     fontSize: 12,
+                   ),
+               ),
                  ),
                ),
+               ListTile(
+                 leading: const Icon(Icons.home),
+                 title: const Text("Home"),
+                 onTap: () {},
                ),
-               
-
+               const Divider(color:Colors.black, thickness: 1),
+               ListTile(
+                 leading: const Icon(Icons.account_box),
+                 title: const Text("About"),
+                 onTap: () {},
+               ),
+               const Divider(color:Colors.black, thickness: 1, endIndent: 1),
+               ListTile(
+                 leading: const Icon(Icons.grid_3x3_outlined),
+                 title: const Text("Products"),
+                 onTap: () {},
+               ),
+               const Divider(color:Colors.black, thickness: 1),
+               ListTile(
+                 leading: const Icon(Icons.contact_mail),
+                 title: const Text("Contact"),
+                 onTap: () {},
+               ),
+               const Divider(color:Colors.black, thickness: 1),
+               Padding(
+                 padding: const EdgeInsets.only(left: 230.0,top: 140.0,right: 0.0,bottom: 0.0),
+                 child: IconButton(onPressed: () {} , icon: const Icon(
+                   Icons.account_circle_sharp,
+                   size: 50,
+                 ),
+                 ),
+               ),
              ],
            ),
          ),
